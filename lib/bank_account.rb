a967ee59660f21f0c83cf222ae727f8fd7df9a42
Katie Hoffman <ktahoffman@gmail.com>
2014-09-16 17:28:00 -0400

    add code back in after master merge

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,27 @@
 class BankAccount
-  # code here
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(deposit_amount)
+    self.balance += deposit_amount
+  end
+
+  def display_balance
+    "Your Balance is $#{balance}."
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
+  def valid?
+    balance > 0 && status == "open"
+  end
+
 end
