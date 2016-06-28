b64a341a6e9fafdb290b7e366e778ab392a66bb4
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-06-28 15:51:21 -0400

    change both_valid? to valid? method to match master branch test suite close issue #20

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,12 +9,12 @@ class Transfer
     @amount = amount
   end
 
-  def both_valid?
+  def valid?
     sender.valid? && receiver.valid?
   end
 
   def execute_transaction
-    if both_valid? && sender.balance > amount && self.status == "pending"
+    if valid? && sender.balance > amount && self.status == "pending"
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
@@ -24,7 +24,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if both_valid? && receiver.balance > amount && self.status == "complete"
+    if valid? && receiver.balance > amount && self.status == "complete"
       receiver.balance -= amount
       sender.balance += amount
       self.status = "reversed"
