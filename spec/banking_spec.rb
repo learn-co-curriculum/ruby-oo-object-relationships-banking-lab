3b7122b036d029b8d5bd997d289e15ac3e250492
jenmyers <jen.myers@flatironschool.com>
2018-09-10 10:29:01 -0500

    Remove extra describe blocks in tests

--- spec/banking_spec.rb
+++ spec/banking_spec.rb
@@ -2,27 +2,25 @@ describe "BankAccount" do
 
   let(:avi) { BankAccount.new("Avi") }
 
-  describe 'BankAccount' do
-    describe "#initialize" do
-      it "can initialize a Bank Account" do
-        expect(avi).to be_a(BankAccount)
-      end
+  describe "#initialize" do
+    it "can initialize a Bank Account" do
+      expect(avi).to be_a(BankAccount)
+    end
 
-      it "initializes with a name" do
-        expect(avi.name).to eq("Avi")
-      end
+    it "initializes with a name" do
+      expect(avi.name).to eq("Avi")
+    end
 
-      it "always initializes with balance of 1000" do
-        expect(avi.balance).to eq(1000)
-      end
+    it "always initializes with balance of 1000" do
+      expect(avi.balance).to eq(1000)
+    end
 
-      it "always initializes with a status of 'open'" do
-        expect(avi.status).to eq("open")
-      end
+    it "always initializes with a status of 'open'" do
+      expect(avi.status).to eq("open")
+    end
 
-      it "can't change its name" do
-        expect { avi.name = "Bob" }.to raise_error
-      end
+    it "can't change its name" do
+      expect { avi.name = "Bob" }.to raise_error
     end
   end
 
