748deb367c2464a5f42431f09d834491d8681051
maxwellbenton <maxwellbenton@gmail.com>
2019-07-01 18:44:13 -0400

    updates test

--- spec/banking_spec.rb
+++ spec/banking_spec.rb
@@ -20,7 +20,7 @@ describe "BankAccount" do
     end
 
     it "can't change its name" do
-      expect { avi.name = "Bob" }.to raise_error(ArgumentError)
+      expect { avi.name = "Bob" }.to raise_error(NoMethodError)
     end
   end
 
