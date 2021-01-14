fb2c40cbfd8df1833be04dff3978de05d6f36b01
David Polcari <dpolcari18@gmail.com>
2021-01-14 10:51:27 -0600

    passing all tests

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+    
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = 'open'
+    end
 
+    def deposit(amount)
+        self.balance += amount
+    end
+
+    def display_balance
+        return "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        if @status == 'open' && @balance > 0
+            true
+        else
+            false
+        end
+    end
+
+    def close_account
+        self.status= 'closed'
+    end
 end
